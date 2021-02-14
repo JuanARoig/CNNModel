bdfccd045fb49ea958c578ea3b232dcf3e7a5188
Juan Roig <jaroig@outlook.com>
2021-02-14 01:22:39 -0500

    adding transitionDimensions

--- CNNModel/properties.cs
+++ CNNModel/properties.cs
@@ -12,6 +12,7 @@ namespace neuralnet
 	    private int[] FCLayerSizes;
 
         //Convolutional Neural Net Properties
+        private int[3] transitionDimensions;//Dimensions boundary in order to transition to FCNN.
         private int[,] kernelSizes;//kernel1Size is the cubic dimensions of the kernel. j-hat should be of size 3.
 	    private List<double[,,]> convLayerNetwork;
 	    private List<double[,,]> convLayerKernel;
