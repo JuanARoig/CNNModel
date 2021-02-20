4dc21a6c8e8ae81b1968ba73d2a39664a1deb555
Juan Roig <jaroig@outlook.com>
2021-02-20 16:13:41 -0500

    starting pderiv funcs

---
+++ CNNModel/costpartialderivs.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace neuralnet
+{
+    partial class ConvolutionalNN
+    {
+        private double CEPDerivKernels() { }
+        private double CEDerivWeights() { }
+        private double CEDerivBiases() { }
+
+        private double MSEDerivKernels() { }
+        private double MSEDerivWeights() { }
+        private double MSEDerivBiases() { }
+    }
+}
