6fc50916724c0860cf05766d692648c0c4eb98db
Juan Roig <jaroig@outlook.com>
2021-02-19 23:09:27 -0500

    starting error functions

--- CNNModel/costfunctions.cs
+++ CNNModel/costfunctions.cs
@@ -7,6 +7,19 @@ namespace neuralnet
 {
     partial class ConvolutionalNN
     {
-        //mean squared error
+        //mean squared error function
+        private double MeanSquaredCost(double[,,] inputData, double[] expectedOutput)
+        {
+            double finalError;
+            double[] output = GeneralCNN(inputData);
+            return finalError;
+        }
+        //cross entropy loss function
+        private double CrossEntropyLoss(double[,,] inputData, double[] expectedOutput)
+        {
+            double finalError;
+            double[] output = GeneralCNN(inputData);
+            return finalError;
+        }
     }
 }
