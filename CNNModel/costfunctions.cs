5aef513c661ed465404a5d12920dae921ed86696
Juan Roig <jaroig@outlook.com>
2021-02-20 15:09:27 -0500

    finishing cost functions

--- CNNModel/costfunctions.cs
+++ CNNModel/costfunctions.cs
@@ -10,15 +10,25 @@ namespace neuralnet
         //mean squared error function
         private double MeanSquaredCost(double[,,] inputData, double[] expectedOutput)
         {
-            double finalError;
+            double finalError = 0;
             double[] output = GeneralCNN(inputData);
+            for (int i = 0; i < output.Length; i++)
+            {
+                finalError += Math.Pow(output[i] - expectedOutput[i], 2);
+            }
+            finalError *= (1/output.Length);
             return finalError;
         }
         //cross entropy loss function
         private double CrossEntropyLoss(double[,,] inputData, double[] expectedOutput)
         {
-            double finalError;
+            double finalError = 0;
             double[] output = GeneralCNN(inputData);
+            for (int i = 0; i < output.Length; i++);
+            {
+                finalError += expectedOutput[i]*(Math.Log10(output[i]));
+            }
+            finalError *= (1/output.Length);
             return finalError;
         }
     }
