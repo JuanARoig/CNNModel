c264a2d7ffa6f67311a24889bb3392a85d929b71
Juan Roig <jaroig@outlook.com>
2021-02-20 17:11:05 -0500

    pderiv changes

--- CNNModel/costpartialderivs.cs
+++ CNNModel/costpartialderivs.cs
@@ -7,12 +7,36 @@ namespace neuralnet
 {
     partial class ConvolutionalNN
     {
-        private double CEPDerivKernels() { }
-        private double CEDerivWeights() { }
-        private double CEDerivBiases() { }
+        private double CEPDerivKernels(double[,,] inputData, double[] expectedOutput, int listpos, int arrpos1, int arrpos2, int arrpos3) 
+        { 
+            double r = 0.00000001;
+            double original = this.CrossEntropyLoss(inputData, expectedOutput);
+        }
+        private double CEDerivWeights(double[,,] inputData, double[] expectedOutput, int listpos, int arrpos1, int arrpos2) 
+        { 
+            double r = 0.00000001;
+            double original = this.CrossEntropyLoss(inputData, expectedOutput);
+        }
+        private double CEDerivBiases(double[,,] inputData, double[] expectedOutput, int listpos, int arrpos) 
+        { 
+            double r = 0.00000001;
+            double original = this.CrossEntropyLoss(inputData, expectedOutput);
+        }
 
-        private double MSEDerivKernels() { }
-        private double MSEDerivWeights() { }
-        private double MSEDerivBiases() { }
+        private double MSEDerivKernels(double[,,] inputData, double[] expectedOutput, int listpos, int arrpos1, int arrpos2, int arrpos3) 
+        { 
+            double r = 0.00000001;
+            double original = this.MeanSquaredCost(inputData, expectedOutput);
+        }
+        private double MSEDerivWeights(double[,,] inputData, double[] expectedOutput, int listpos, int arrpos1, int arrpos2) 
+        { 
+            double r = 0.00000001;
+            double original = this.MeanSquaredCost(inputData, expectedOutput);
+        }
+        private double MSEDerivBiases(double[,,] inputData, double[] expectedOutput, int listpos, int arrpos) 
+        { 
+            double r = 0.00000001;
+            double original = this.MeanSquaredCost(inputData, expectedOutput);
+        }
     }
 }
