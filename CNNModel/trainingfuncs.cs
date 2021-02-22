fdf7e5cc971dec917cd9083a59356a513de65412
Juan Roig <jaroig@outlook.com>
2021-02-22 12:32:23 -0500

    creating file for training funcs

---
+++ CNNModel/trainingfuncs.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace neuralnet
+{
+    partial class ConvolutionalNN
+    {
+        //use partialderivs to implement gradient descent in backpropagation
+    }
+}
