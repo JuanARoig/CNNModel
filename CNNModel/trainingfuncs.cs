b722e9f20c7dea35655f4b8332f1555a14136490
Juan Roig <jaroig@outlook.com>
2021-02-23 12:46:43 -0500

    correction to training function start

--- CNNModel/trainingfuncs.cs
+++ CNNModel/trainingfuncs.cs
@@ -8,24 +8,28 @@ namespace neuralnet
     partial class ConvolutionalNN
     {
         //use partialderivs to implement gradient descent in backpropagation
-        public void CrossEntropyTraining(List<double[,,] inputDataList, List<double[]> outputDataList)
+        public void CrossEntropyTraining(double learningRate, List<double[,,] inputDataList, List<double[]> outputDataList)
         {
             foreach(double[,,] inputData, double[] expectedOutput in inputDataList, outputDataList)
             {
-                foreach()
+                
+                double original = this.CrossEntropyLoss(inputData, expectedOutput);
+                for (int x = 0; x < this.convLayerKernels.Capacity; x++)
                 {
-                    double original = this.CrossEntropyLoss(inputData, expectedOutput);
                     
                 }
+                
             }
         }
         public void MeanSquaredTraining(List<double[]> inputDataList, List<double[]> outputDataList)
         {
             foreach(double[,,] inputData, double[] expectedOutput in inputDataList, outputDataList)
             {
-                foreach()
+            
+                double original = this.MeanSquaredCost(inputData, expectedOutput);
+                for (int x = 0; x < this.convLayerKernels.Capacity; x++)
                 {
-                    double original = this.MeanSquaredCost(inputData, expectedOutput);
+                    
                 }
             }
         }
