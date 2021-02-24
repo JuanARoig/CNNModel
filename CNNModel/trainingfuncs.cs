b66d76b0f9fbcd87cd7de6f3bb878f36c41bcdd6
Juan Roig <jaroig@outlook.com>
2021-02-24 12:41:01 -0500

    iteration over dataset

--- CNNModel/trainingfuncs.cs
+++ CNNModel/trainingfuncs.cs
@@ -10,25 +10,89 @@ namespace neuralnet
         //use partialderivs to implement gradient descent in backpropagation
         public void CrossEntropyTraining(double learningRate, List<double[,,]> inputDataList, List<double[]> outputDataList)
         {
-            foreach(double[,,] inputData, double[] expectedOutput in inputDataList, outputDataList)
+            for (int l = 0; l < 15; l++)
             {
-                
-                double original = this.CrossEntropyLoss(inputData, expectedOutput);
-                for (int x = 0; x < this.convLayerKernels.Capacity; x++)
+                foreach(double[,,] inputData, double[] expectedOutput in inputDataList, outputDataList)
                 {
-                    
+                
+                    double original = this.CrossEntropyLoss(inputData, expectedOutput);
+                    for (int x = 0; x < this.convLayerKernels.Capacity; x++)
+                    {
+                        double[,,] currentKernel = this.convLayerKernels[x];
+                        for (int i = 0; i < currentKernel.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < currentKernel.GetLength(1); j++)
+                            {
+                                for (int k = 0; k < currentKernel.GetLength(2); k++)
+                                {
+                                
+                                }
+                            }
+                        }
+                    }
+                    for (int x = 0; x < this.weights.Capacity; x++)
+                    {
+                        double[,] currentWeights = this.weights[x];
+                        for (int i = 0; i < currentWeights.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < currentWeights.GetLength(1); j++)
+                            {
+
+                            }
+                        }
+                    }
+                    for (int x = 0; x < this.biases.Capacity; x++)
+                    {
+                        double[] currentBiases = this.biases[x];
+                        for (int i = 0; i < currentBiases.Length; i++)
+                        {
+
+                        }
+                    }
                 }
             }
         }
         public void MeanSquaredTraining(List<double[]> inputDataList, List<double[]> outputDataList)
         {
-            foreach(double[,,] inputData, double[] expectedOutput in inputDataList, outputDataList)
+            for (int l = 0; l < 15; l++)
             {
-            
-                double original = this.MeanSquaredCost(inputData, expectedOutput);
-                for (int x = 0; x < this.convLayerKernels.Capacity; x++)
+                foreach(double[,,] inputData, double[] expectedOutput in inputDataList, outputDataList)
                 {
-                    
+            
+                    double original = this.MeanSquaredCost(inputData, expectedOutput);
+                    for (int x = 0; x < this.convLayerKernels.Capacity; x++)
+                    {
+                        double[,] currentKernel = this.convLayerKernels[x];
+                        for (int i = 0; i < currentKernel.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < currentKernel.GetLength(1); j++)
+                            {
+                                for (int k = 0; k < currentKernel.GetLength(2); k++)
+                                {
+                                
+                                }
+                            }
+                        }
+                    }
+                    for (int x = 0; x < this.weights.Capacity; x++)
+                    {
+                        double[,] currentWeights = this.weights[x];
+                        for (int i = 0; i < currentWeights.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < currentWeights.GetLength(1); j++)
+                            {
+
+                            }
+                        }
+                    }
+                    for (int x = 0; x < this.biases.Capacity; x++)
+                    {
+                        double[] currentBiases = this.biases[x];
+                        for (int i = 0; i < currentBiases.Length; i++)
+                        {
+
+                        }
+                    }
                 }
             }
         }
