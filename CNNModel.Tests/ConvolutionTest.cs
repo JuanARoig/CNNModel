cc509e60abec0f16a156636c17c1561db18f14a4
Juan Roig <jaroig@outlook.com>
2021-03-02 17:20:53 -0500

    adding more tests

--- CNNModel.Tests/ConvolutionTest.cs
+++ CNNModel.Tests/ConvolutionTest.cs
@@ -6,19 +6,44 @@ namespace CNNModel.Tests
     public class CNNTests
     {
     	[Fact]
-    	public void ConvolutionTest(double[,,] inputTensor, double[,,] kernel)
+    	public void ConvolutionTest(double[,,] testInputTensor, double[,,] testKernel)
     	{
 		
     	}
 		[Fact]
-		public void AvgPoolingTest(double[,,] inputTensor, double[] prevKernelDimensions)
+		public void AvgPoolingTest(double[,,] testInputTensor, double[] testPrevKernelDimensions)
 		{
 
 		}
 		[Fact]
-		public void MaxPoolingTest(double[,,] inputTensor, double[] prevKernelDimensions)
+		public void MaxPoolingTest(double[,,] testInputTensor, double[] testPrevKernelDimensions)
 		{
 
+		}
+		[Fact]
+		public void REluTest(double[,,] testInput)
+		{
+
+		}
+		[Fact]
+		public void SoftmaxTest(double[] testInput)
+		{
+			
+		}
+		[Fact]
+		public void MatrixVectorProductTest(double[] testInput)
+		{
+			
+		}
+		[Fact]
+		public void AddTest(double[] testInput)
+		{
+			
+		}
+		[Fact]
+		public void GeneralCNN(double[,,] testInput)
+		{
+			
 		}
     }
 }
