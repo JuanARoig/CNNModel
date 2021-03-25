152febe9141be49f606eeb960bce03d5bbde83e5
Juan Andres Roig <juanroig@localhost.localdomain>
2021-03-25 13:24:48 -0400

    re-formatting utests

--- CNNModel.Tests/CNNModelTests.cs
+++ CNNModel.Tests/CNNModelTests.cs
@@ -10,19 +10,49 @@ namespace CNNModel.Tests
     public class CNNTests : ConvolutionalNN
     {
     	[Fact]
-    	public void ConvolutionTest()
+    	public void Convolution_Evaluates_Input1()
+    	{
+			
+    	}
+		[Fact]
+    	public void Convolution_Evaluates_Input2()
     	{
 		
     	}
 		[Fact]
-		public void AvgPoolingTest()
+		public void Convolution_Throws_Exception()
 		{
 
 		}
 		[Fact]
-		public void MaxPoolingTest()
+		public void AvgPooling_Evaluates_Input1()
 		{
 
+		}
+		[Fact]
+		public void AvgPooling_Evaluates_Input2()
+		{
+
+		}
+		[Fact]
+		public void AvgPooling_Throws_Exception()
+		{
+
+		}
+		[Fact]
+		public void MaxPooling_Evaluates_Input1()
+		{
+
+		}
+		[Fact]
+		public void MaxPooling_Evaluates_Input2()
+		{
+
+		}
+		[Fact]
+		public void MAxPooling_Throws_Exception()
+		{
+			
 		}
 		[Fact]
 		public void RElu_Evaluates_Input1()
@@ -40,22 +70,62 @@ namespace CNNModel.Tests
 			Assert.Throws<Exception>(() => REluOperation(new double[,,] {{{}}}));
 		}
 		[Fact]
-		public void SoftmaxTest()
+		public void SoftmaxEvaluates_Input1()
 		{
 			
 		}
 		[Fact]
-		public void MatrixVectorProductTest()
+		public void SoftmaxEvaluates_Input2()
 		{
 			
 		}
 		[Fact]
-		public void AddTest()
+		public void Softmax_Throws_Exception()
+		{
+
+		}
+		[Fact]
+		public void MatrixVectorProduct_Evaluates_Input1()
 		{
 			
 		}
 		[Fact]
-		public void GeneralCNN()
+		public void MatrixVectorProduct_Evaluates_Input2()
+		{
+			
+		}
+		[Fact]
+		public void MatrixVectorProduct_Throws_Exception()
+		{
+
+		}
+		[Fact]
+		public void Add_Evaluates_Input1()
+		{
+			
+		}
+		[Fact]
+		public void Add_Evaluates_Input2()
+		{
+			
+		}
+		[Fact]
+		public void Add_Throws_Exception()
+		{
+
+		}
+		[Fact]
+		public void GeneralCNN_Evaluates_Input1()
+		{
+			
+		}
+		[Fact]
+		public void GeneralCNN_Evaluates_Input2()
+		{
+			
+		}
+		[Fact]
+		public void GeneralCNN_Throws_Exception()
 		{
 
 		}
